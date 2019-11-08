de6a8bf59fe345d9e4fc7851d3cd86a5a21862ee
PUPPY <thanh.nn1106@gmail.com>
2019-11-09 01:59:30 +0800

    update

--- resources/views/theme/modern/partials/homepage_image_carousel.blade.php
+++ resources/views/theme/modern/partials/homepage_image_carousel.blade.php
@@ -4,17 +4,17 @@
             <div id="owl-demo" class="owl-demo themeqx-carousel-ads">
                 <div class="item">
                     <a href="{{ url('thong-tin/ve-chung-toi') }}">
-                        <img src="https://xachtayonline-vn.s3-ap-southeast-1.amazonaws.com/homepage-banner/banner1.jpg" alt="xachtayonline.vn">
+                        <img src="https://xachtayonline-vn.s3-ap-southeast-1.amazonaws.com/homepage-banner/banner-1.jpg" alt="xachtayonline.vn">
                     </a>
                 </div>
                 <div class="item">
                     <a href="{{ url('thong-tin/ve-chung-toi') }}">
-                        <img src="https://xachtayonline-vn.s3-ap-southeast-1.amazonaws.com/homepage-banner/banner2.png" alt="xachtayonline.vn">
+                        <img src="https://xachtayonline-vn.s3-ap-southeast-1.amazonaws.com/homepage-banner/banner-2.jpg" alt="xachtayonline.vn">
                     </a>
                 </div>
                 <div class="item">
-                    <a href="{{ url('thong-tin/ve-chung-toi') }}">
-                        <img src="https://xachtayonline-vn.s3-ap-southeast-1.amazonaws.com/homepage-banner/banner2.png" alt="xachtayonline.vn">
+                    <a href="http://xachtayonline.vn/danh-sach?q=tinh+d%E1%BA%A7u+th%C6%A1m">
+                        <img src="https://xachtayonline-vn.s3-ap-southeast-1.amazonaws.com/homepage-banner/banner-3.jpg" alt="xachtayonline.vn">
                     </a>
                 </div>
             </div>
